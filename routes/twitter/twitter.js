d2349d1764c210f4249da21d412a35635a29a5aa
mentos1 <kovaloff1995@gmail.com>
2019-03-18 16:43:58 +0200

    add lib fs

--- routes/twitter/twitter.js
+++ routes/twitter/twitter.js
@@ -5,7 +5,7 @@ const Token = require('../../bin/Services/token');
 const request = require('request');
 
 /* GET users listing. */
-router.post('/', (req, res, next) => {
+router.get('/', (req, res, next) => {
     console.log(req.query);
     request.post({
         url: `https://api.twitter.com/oauth/access_token?oauth_verifier`,
