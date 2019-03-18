159a68fb8204ed2833a7e853cf1718931af1416e
mentos1 <kovaloff1995@gmail.com>
2019-03-18 12:17:59 +0200

    add lib fs

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var auth_twitter = require('./routes/auth_twitter');
 var auth_vk = require('./routes/auth_vk');
 var check = require('./routes/check');
-
+const cors = require('cors');
 var app = express();
 
 // view engine setup
@@ -16,7 +16,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -25,19 +25,19 @@ app.use('/auth_vk', auth_vk);
 app.use('/check', check);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 // enable cors
