dde119c4bd48a8efa44da96c1bb4fc8589ae7ab7
mentos1 <kovaloff1995@gmail.com>
2019-03-20 19:16:48 +0200

    add Socket Singleton

--- routes/twitter/twitter_reverse.js
+++ routes/twitter/twitter_reverse.js
@@ -8,7 +8,7 @@ router.get('/', passport.authenticate('twitter', { failureRedirect: '/login' }),
     function(req, res) {
         // Successful authentication, redirect home.
         console.error('All right')
-        console.log(SocketSingleton.io);
+        //console.log(SocketSingleton.io);
         console.error('All right')
         SocketSingleton.io.emit('twitter', req.user); //.in(req.session.socketId)
         res.end()
