71d7fab72e446f69233f8a6c5fc7bc9beeb87492
mentos1 <kovaloff1995@gmail.com>
2019-03-20 18:51:05 +0200

    add passport-twitter

--- routes/twitter/twitter_reverse.js
+++ routes/twitter/twitter_reverse.js
@@ -8,7 +8,9 @@ router.get('/', passport.authenticate('twitter', { failureRedirect: '/login' }),
     function(req, res) {
         // Successful authentication, redirect home.
         console.error('All right')
-        console.error(req)
+        console.error(req.session.socketId);
+        console.error(req.user);
+        console.error(io);
         console.error('All right')
         io.in(req.session.socketId).emit('user', req.user)
         res.end()
