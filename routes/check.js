f7179d3dd89f113a0e0d68bd6b3be643e46c65d6
mentos1 <kovaloff1995@gmail.com>
2019-03-21 12:26:53 +0200

    add Socket Singleton

--- routes/check.js
+++ routes/check.js
@@ -4,9 +4,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     const io = req.app.get('io')
-    console.error('All right')
-    console.log(io);
-    console.error('All right');
     io.emit('twitter', 'test1_1');
     res.send({"success" : true});
 });
