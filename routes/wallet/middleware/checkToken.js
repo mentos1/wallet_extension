7168bfa7a151e07259555c7372c4f0451f56b26d
mentos1 <kovaloff1995@gmail.com>
2019-03-21 15:39:22 +0200

    install web3

--- routes/wallet/middleware/checkToken.js
+++ routes/wallet/middleware/checkToken.js
@@ -2,12 +2,16 @@ const {UserRepositories} = require('../../../bin/Repositories/index');
 
 
 const isToken = async function (req, res, next) {
-    let isToken = await UserRepositories.isToken(req.body.token)
-     if (isToken) {
-         next()
-     } else {
-         return res.send(304, 'Token is Invalid');
-     }
+    console.log('_________!!!!!!!!!!!!!__________________');
+    console.log(req.body, req.body.token);
+    console.log('_________!!!!!!!!!!!!!__________________');
+
+    let isToken = await UserRepositories.isToken(req.body.token);
+    if (isToken) {
+        next()
+    } else {
+        return res.send(304, 'Token is Invalid');
+    }
 };
 
 
