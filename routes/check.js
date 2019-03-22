01f34660a4447987fd4a0b47ac778b65eba6efd9
mentos1 <kovaloff1995@gmail.com>
2019-03-22 14:55:20 +0200

    add twitter lib api

--- routes/check.js
+++ routes/check.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const {getFriends} = require('../bin/Services/twitter');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    getFriends(1083652953083396098);
     res.send({"success" : true});
 });
 
