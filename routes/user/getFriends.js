3f46e1290134886be49ae0907d593fbc036efce7
mentos1 <kovaloff1995@gmail.com>
2019-03-23 15:32:12 +0200

    add twitter lib api

--- routes/user/getFriends.js
+++ routes/user/getFriends.js
@@ -11,14 +11,8 @@ router.post('/', middleware.isToken, async function(req, res, next) {
     try {
         let user = await UserRepositories.getUserByToken(req.body.token);
         if (user) {
-            console.log('________!0_______________');
-            console.log(user, user.screen_name);
-            console.log('________!0_______________');
             let filteredUser = await getFriendsList(user.screen_name);
-                console.log('________!1_______________');
-                console.log(filteredUser); //todo cat info
-                console.log('________!1_______________');
-            return res.status(200).send(filteredUser);
+            return res.status(200).send({friends :filteredUser});
         } else {
             return res.send(500, 'Error get user');
         }
