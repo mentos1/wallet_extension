8432e5e1ac4e4fa9411e0cc26ed04df7e86e4db4
mentos1 <kovaloff1995@gmail.com>
2019-03-23 19:12:02 +0200

    add twitter lib api

--- routes/wallet/sendEth.js
+++ routes/wallet/sendEth.js
@@ -13,23 +13,25 @@ router.post('/', middleware.isToken, async function (req, res, next) {
         let user = await UserRepositories.getUserByToken(req.body.token);
         let address = await createById(this.body.user_id);
 
-        let pk = user.wallets && Object.values(user.wallets).length ? Object.keys(user.wallets)[0] : null,
+        let pk = user.wallets && Object.values(user.wallets).length ? Object.values(user.wallets)[0] : null,
             amount = req.body.amount;
 
         if (!amount || !address || !pk) {
-            return res.send(500, 'Error params');
+            return res.status(500).send({status : 0, msg: 'Error params'})
+
         }
 
         let response = await sendTransaction(pk, address, amount);
 
         if (response.status) {
-            return res.status(200).send({hash: response.data})
+            return res.status(200).send({status : 1, msg: response.data})
         } else {
-            return res.send(500, response.msg);
+            return res.status(500).send({status : 0, msg: response.msg})
+
         }
 
     } catch (e) {
-        return res.send(500, 'Error insert pk');
+        return res.status(500).send({status : 0, msg: e})
     }
 
 });
