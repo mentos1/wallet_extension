e8af50e6ee913eb044e623b2903109afab3f3b9c
mentos1 <kovaloff1995@gmail.com>
2019-03-25 11:52:53 +0200

    add twitter lib api

--- routes/middleware/checkToken.js
+++ routes/middleware/checkToken.js
@@ -7,13 +7,16 @@ const isToken = async function (req, res, next) {
     console.log('_________!!!!!!!!!!!!!__________________');
 
     if (!Object.values(req.body).length) {
+        console.log('Body is empty');
         return res.send(304, 'Body is empty');
     }
-
+    console.log(2);
     let isToken = await UserRepositories.isToken(req.body.token);
     if (isToken) {
+        console.log('next');
         return next()
     } else {
+        console.log('Token is Invalid');
         return res.send(304, 'Token is Invalid');
     }
 };
