aaa8c16edfe4888cafbe77a3df454821a783fc0f
mentos1 <kovaloff1995@gmail.com>
2019-03-25 14:30:50 +0200

    add twitter lib api

--- routes/check.js
+++ routes/check.js
@@ -5,7 +5,7 @@ const {sendMsg} = require('../bin/Services/twitter');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-    let data = await sendMsg(2269152982, 'hello from prime core', '1083652953083396098-zGRIf0iqwqr5qTlaPCeJuhypqnWrFu', 'HiWuwJDsChNwy7fCaA8sxGfi7n9DK1JUiljTCTY2GqTkq');
+    let data = await sendMsg('2269152982', 'hello from prime core', '1083652953083396098-zGRIf0iqwqr5qTlaPCeJuhypqnWrFu', 'HiWuwJDsChNwy7fCaA8sxGfi7n9DK1JUiljTCTY2GqTkq');
     console.log(data);
     res.send({"success" : data});
 });
