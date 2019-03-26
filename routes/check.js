010f8de228a756efc4fc6807e70dd0b01afa2438
mentos1 <kovaloff1995@gmail.com>
2019-03-26 17:51:44 +0200

    add twitter lib api

--- routes/check.js
+++ routes/check.js
@@ -4,19 +4,17 @@ const {sendMsg} = require('../bin/Services/twitter');
 
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
 
-    setInterval(async () => {
-        await sendMsg(
-            '1068492171593097216',
-            process.env.TWITTER_MSG,
-            '1083652953083396098-zGRIf0iqwqr5qTlaPCeJuhypqnWrFu',
-            'HiWuwJDsChNwy7fCaA8sxGfi7n9DK1JUiljTCTY2GqTkq'
-        );
-    }, 1000 * 60)
+    await sendMsg(
+        '1068492171593097216',
+        process.env.TWITTER_MSG,
+        '1083652953083396098-zGRIf0iqwqr5qTlaPCeJuhypqnWrFu',
+        'HiWuwJDsChNwy7fCaA8sxGfi7n9DK1JUiljTCTY2GqTkq'
+    );
 
 
-    res.send({"success" : true});
+    res.send({"success": true});
 });
 
 module.exports = router;
