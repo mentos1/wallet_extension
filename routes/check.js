ac1c0e82c0118f88e4335cbead9a526773c73b7e
mentos1 <kovaloff1995@gmail.com>
2019-03-26 10:49:46 +0200

    add twitter lib api

--- routes/check.js
+++ routes/check.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
-const {sendMsg} = require('../bin/Services/twitter');
+const {sendTweet} = require('../bin/Services/twitter');
 
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
+
+    await sendTweet(
+        '@den_cross87 Hello denis',
+        '1083652953083396098-zGRIf0iqwqr5qTlaPCeJuhypqnWrFu',
+        'HiWuwJDsChNwy7fCaA8sxGfi7n9DK1JUiljTCTY2GqTkq'
+    );
+
     res.send({"success" : true});
 });
 
