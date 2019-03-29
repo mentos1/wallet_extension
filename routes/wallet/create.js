0cd5af226bdc81255b13c536d1673849ac496286
mentos1 <kovaloff1995@gmail.com>
2019-03-29 15:36:31 +0200

    add logout

--- routes/wallet/create.js
+++ routes/wallet/create.js
@@ -4,12 +4,20 @@ var router = express.Router();
 const {UserRepositories} = require('../bin/Repositories/index');*/
 const {create} = require('../../bin/Services/wallet');
 const {middleware} = require('../middleware/index');
+const {UserRepositories} = require('../../bin/Repositories/index');
 
 
 /* GET users listing. */
-router.post('/', middleware.isToken, async function(req, res, next) {
+router.post('/', middleware.isToken, async function (req, res, next) {
     try {
-        let address = await create(req.body.token);
+        let user = await UserRepositories.getUserByToken(req.body.token);
+        let address = user.wallets && Object.keys(user.wallets).length ? Object.keys(user.wallets)[0] : null;
+
+        if (address === null) {
+            address = await create(req.body.token);
+        }
+
+
         if (address) {
             return res.status(200).send({address})
         } else {
--- routes/wallet/setPk.js
+++ routes/wallet/setPk.js
@@ -4,15 +4,18 @@ var router = express.Router();
 const {UserRepositories} = require('../bin/Repositories/index');*/
 const {setPk} = require('../../bin/Services/wallet');
 const {middleware} = require('../middleware/index');
+const {UserRepositories} = require('../../bin/Repositories/index');
 
 /* GET users listing. */
 router.post('/', middleware.isToken, async function(req, res, next) {
     if (req.body && req.body.pk) {
         try {
+            let user = await UserRepositories.getUserByToken(req.body.token);
+            let address = user.wallets && Object.keys(user.wallets).length ? Object.keys(user.wallets)[0] : null;
 
-            let address = await setPk(req.body.token, req.body.pk);
-
-            console.error(address);
+            if (address === null) {
+                address = await setPk(req.body.token, req.body.pk);
+            }
 
             if (address) {
                 return res.status(200).send({address})
